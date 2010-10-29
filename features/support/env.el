3b2794392a508d2fd7387a7f070f3b50ec5bbf6a
Johan Andersson <johan.rejeep@gmail.com>
2010-10-29 12:06:47 +0200

    Added necessary stuff for running Ecukes.

---
+++ features/support/env.el
@@ -0,0 +1,13 @@
+(let* ((current-directory (file-name-directory load-file-name))
+       (features-directory (expand-file-name ".." current-directory))
+       (project-directory (expand-file-name ".." features-directory)))
+  (setq ruby-end-root-path project-directory)
+  (setq ruby-end-util-path (expand-file-name "util" project-directory)))
+
+(add-to-list 'load-path ruby-end-root-path)
+(add-to-list 'load-path (expand-file-name "espuds" ruby-end-util-path))
+(add-to-list 'load-path (expand-file-name "emacs-lisp" (expand-file-name "lisp" (expand-file-name "ert" ruby-end-util-path))))
+
+(require 'ruby-end)
+(require 'espuds)
+(require 'ert)
