be0e1a154f3776e91c0cc6f1eba1c2ea551a1ea4
Johan Andersson <johan.rejeep@gmail.com>
2012-06-19 08:04:43 +0200

    Update copyright years.

--- ruby-end.el
+++ ruby-end.el
@@ -1,6 +1,6 @@
 ;;; ruby-end.el --- Automatic insertion of end blocks for Ruby
 
-;; Copyright (C) 2010 Johan Andersson
+;; Copyright (C) 2010-2012 Johan Andersson
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
