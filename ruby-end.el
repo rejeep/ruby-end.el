2d4097afcb89372441c28e33011dd361701d1c98
Dmitry Gutov <dgutov@yandex.ru>
2023-01-26 02:40:29 +0200

    Update the copyright blurb
    #17

--- ruby-end.el
+++ ruby-end.el
@@ -1,6 +1,6 @@
 ;;; ruby-end.el --- Automatic insertion of end blocks for Ruby
 
-;; Copyright (C) 2010-2012 Johan Andersson
+;; Copyright (C) 2010-2015, 2017, 2023  Free Software Foundation, Inc.
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
@@ -8,26 +8,20 @@
 ;; Keywords: speed, convenience, ruby
 ;; URL: http://github.com/rejeep/ruby-end
 
-;; This file is NOT part of GNU Emacs.
+;; This file is part of GNU Emacs.
 
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
